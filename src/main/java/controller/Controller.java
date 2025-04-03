625b1c41b08a75573602e4eab09c843db2e67c0d
Sneupi <gabevenegas527@gmail.com>
2025-04-03 10:25:14 -0700

    initial maven

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,12 @@
+package controller;
+import model.Model; // Importing Model class
+import view.View; // Importing View class
+
+public class Controller {
+    public static void main(String[] args) {
+        // FIXME: placeholder code
+        Model model = new Model();
+        View view = new View();
+        System.out.println("Hello, World!");
+    }
+}
