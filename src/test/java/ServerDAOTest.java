8f02d44252fb37d3b492c2226854ef73b4e87f21
Sneupi <gabevenegas527@gmail.com>
2025-04-07 11:04:31 -0700

    junit basic

---
+++ src/test/java/ServerDAOTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import model.Server;
+import dao.ServerDAO;
+
+public class ServerDAOTest {
+    @Test
+    public void exampleTestPass() {
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void exampleTestFail() {
+        assertEquals(true, false);
+    }
+}
