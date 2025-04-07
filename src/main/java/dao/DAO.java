4011942e2e8572eda72e4fee82f0e6eb48c459e2
Sneupi <gabevenegas527@gmail.com>
2025-04-07 14:42:26 -0700

    server dao and tests

---
+++ src/main/java/dao/DAO.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.util.List;
+
+/**
+ * DAO is a Data Access Object interface, allowing CRUD
+ * operations between database entities and Java objects.
+ * T is the type of the entity, ID is the id value type.
+ */
+public interface DAO<T, ID> {
+
+    void insert(T entity, DBM db);
+
+    void update(T entity, DBM db);
+
+    T select(ID id, DBM db);
+
+    List<T> selectAll(DBM db);
+
+    void delete(ID id, DBM db);
+}
--- src/main/java/model/Server.java
+++ src/main/java/model/Server.java
@@ -4,26 +4,28 @@ public final class Server {
 	// INSTANCE VARIABLES
 	private final String username;
 	private final String password;
-	
+
 	// CONSTRUCTOR
 	public Server(String username, String password) {
 		this.username = username;
 		this.password = password;		
 	}
-	
+    
 	// GETTERS
 	public String getUsername() {
 		return username;
 	}
-	
+
 	//add a getPassword method
 	public String getPassword() {
 		return password;
 	}
-	
+
 	@Override
 	public String toString() {
-	    return "Server{username='" + username + "'}";
+		return "Server{" +
+				"username='" + username + '\'' +
+				", password='" + password + '\'' +
+				'}';
 	}
-
 }
